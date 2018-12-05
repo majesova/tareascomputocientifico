3b6cc0cb7a37f4c858e0d6dd1c72c927ef6ef8d2
MANUEL SOBERANIS <majesova@gmail.com>
2018-12-04 23:23:46 -0600

    ejercicio 3 completo y se agregan factories

--- Names.py
+++ Names.py
@@ -1,11 +1,87 @@
-f=open("names.txt")
-char_num_dict={"A":1,"B":2,"C":3,"D":4,"E":5,"F":6,"G":7,"H":8,"I":9,"J":10,"K":11,"L":12,"M":13,"N":14,"O":15,"P":16,"Q":17,"R":18,"S":19,"T":20,"U":21,"V":22,"W":23,"X":24,"Y":25,"Z":26}
-names=sorted(f.read().replace('"','').split(','),key=str)
-sum=0
-for ind,val in enumerate(names):
-	temp=0
-	for x in val:
-		temp+=char_num_dict[x]
-	sum+=temp*(ind+1)
-	
-print(sum)
+import time
+def bubbleSort(alist):
+    for passnum in range(len(alist)-1,0,-1):
+        for i in range(passnum):
+            if alist[i]>alist[i+1]:
+                temp = alist[i]
+                alist[i] = alist[i+1]
+                alist[i+1] = temp
+    return alist
+
+def mergeSort(x):
+    result = []
+    if len(x) < 20:
+        return sorted(x)
+    mid = int(len(x) / 2)
+    y = mergeSort(x[:mid])
+    z = mergeSort(x[mid:])
+    i = 0
+    j = 0
+    while i < len(y) and j < len(z):
+        if y[i] > z[j]:
+            result.append(z[j])
+            j += 1
+        else:
+            result.append(y[i])
+            i += 1
+    result += y[i:]
+    result += z[j:]
+    return result
+
+def quicksort(x):
+    if len(x) == 1 or len(x) == 0:
+        return x
+    else:
+        pivot = x[0]
+        i = 0
+        for j in range(len(x)-1):
+            if x[j+1] < pivot:
+                x[j+1],x[i+1] = x[i+1], x[j+1]
+                i += 1
+        x[0],x[i] = x[i],x[0]
+        first_part = quicksort(x[:i])
+        second_part = quicksort(x[i+1:])
+        first_part.append(x[i])
+        return first_part + second_part
+
+def sortList(alist,sortMethod):
+    if sortMethod == "BUBLE":
+        return bubbleSort(alist)
+    if sortMethod == "QUICK":
+        return quicksort(alist)
+    if sortMethod == "MERGE":
+        return mergeSort(alist)
+    if sortMethod == "PYTHON":
+        return sorted(alist,key=str)
+
+def sortAndSum(sortMethod):
+    f=open("names.txt")
+    char_num_dict = {"A":1,"B":2,"C":3,"D":4,"E":5,"F":6,"G":7,"H":8,"I":9,"J":10,"K":11,"L":12,"M":13,"N":14,"O":15,"P":16,"Q":17,"R":18,"S":19,"T":20,"U":21,"V":22,"W":23,"X":24,"Y":25,"Z":26}
+    names = sortList(f.read().replace('"','').split(','),sortMethod)
+    sum=0
+    for ind,val in enumerate(names):
+	    temp=0
+	    for x in val:
+		    temp+=char_num_dict[x]
+	    sum+=temp*(ind+1)
+    return sum
+
+start = time.time()
+result = sortAndSum("BUBLE")
+elapsed = time.time() - start
+print("BUBLE METHOD %s, seconds: %s " % (result ,elapsed))
+
+start = time.time()
+result = sortAndSum("QUICK")
+elapsed = time.time() - start
+print("QUICKSORT METHOD %s, seconds: %s " % (result ,elapsed))
+
+start = time.time()
+result = sortAndSum("MERGE")
+elapsed = time.time() - start
+print("MERGE METHOD %s, seconds: %s " % (result ,elapsed))
+
+start = time.time()
+result = sortAndSum("PYTHON")
+elapsed = time.time() - start
+print("PYTHON METHOD %s, seconds: %s " % (result ,elapsed))
--- Triangle.py
+++ Triangle.py
@@ -1,11 +1,29 @@
 import time
+def getMax(values):
+    greater = values[0] #todas las filas tienen al menos un elemento
+    numValues = len(values)
 
-def sumInRow(rowValue, rowNum):
+    if(numValues == 1):
+        greater =  values[0]
+        return greater
+    
+    for i in range(0, numValues):
+        if values[i] > greater:
+            greater = values[i]
+    return greater
+
+def maxByMethod(values, method):
+    if method == "CUSTOM":
+        return getMax(values)
+    if method == "PYTHON":
+        return max(values)
+
+def sumInRow(rowValue, rowNum, method):
     numElements = len(rowValue[rowNum])
     for i in range(numElements):
-        rowValue[rowNum][i] += max([rowValue[rowNum+1][i], rowValue[rowNum+1][i+1]])
+        rowValue[rowNum][i] += maxByMethod([rowValue[rowNum+1][i], rowValue[rowNum+1][i+1]],method)
     if len(rowValue[rowNum])==1: return rowValue[rowNum][0]
-    else: return sumInRow(rowValue, rowNum-1)
+    else: return sumInRow(rowValue, rowNum-1, method)
 
 rows = []
 with open('triangle.txt') as file:
@@ -13,18 +31,15 @@ with open('triangle.txt') as file:
         rows.append([int(i) for i in line.rstrip('\n').split(" ")])
  
 start = time.time()
-result = sumInRow(rows, len(rows)-2) # start at second to last row
+result = sumInRow(rows, len(rows)-2, "CUSTOM") # start at second to last row
 elapsed = time.time() - start
- 
-print("%s, seconds: %s " % (result ,elapsed))
+print("CUSTOM METHOD: %s, seconds: %s " % (result ,elapsed))
 
-#def sumInRow(data, rowNumber):
+start = time.time()
+result = sumInRow(rows, len(rows)-2, "PYTHON") # start at second to last row
+elapsed = time.time() - start
+print("PYTHON METHOD: %s, seconds: %s " % (result ,elapsed))
 
-
-#triangle = open("triangle.txt")
-#lineIde = 0
-#for line in triangle:
-#    print(len(line))
-#    for i in range(0, len(line)):
-
+#usando getMax: 1074, seconds: 0.0007290840148925781
+#usando max: 1074, seconds: 0.00014901161193847656
         
